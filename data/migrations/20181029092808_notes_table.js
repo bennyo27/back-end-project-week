ce42022e0ec16ea7eaeeb5ecdfc86b39a0b857e5
benny <oseguerabenny27@gmail.com>
2018-10-29 09:40:49 -0700

    Done with basic setup

---
+++ data/migrations/20181029092808_notes_table.js
@@ -0,0 +1,9 @@
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable("notes", tbl => {
+    tbl.increments(); // id
+    tbl.string("title").notNullable();
+    tbl.string("content").notNullable();
+  });
+};
+
+exports.down = function(knex, Promise) {};
---
+++ server/server.js
@@ -0,0 +1,13 @@
+// imports
+const express = require("express");
+const cors = require("cors");
+
+// routes
+const configureRoutes = require("../config/routes");
+
+// instantiate server
+const server = express();
+
+// server use
+server.use(express.json());
+server.use(cors());
